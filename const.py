561a2fa41303549ba7978aa1c286e8e3b7f7fff8
y98aw9dh9awhd9q <b.enbears366@gmail.com>
2026-05-22 14:11:10 -0400

    2026-05-22 keyandno

--- const.py
+++ const.py
@@ -21,5 +21,4 @@ mapDirs  = os.path.join(baseDir, "assets","maps")
 chest    = os.path.join(mapDirs, "chest.png")
 elevator = os.path.join(mapDirs, "elevator.png")
 
-# Xavier is watching me
 # keyano is a poo poo head
